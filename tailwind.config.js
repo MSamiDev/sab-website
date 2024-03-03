ac30ac1d2e439b95453199c7d3c6d290427f1fb0
Mohammad Sami <mohammadsami@duck.com>
2024-03-03 15:47:56 +0530

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ export default {
       "./layouts/**/*.vue",
       "./pages/**/*.vue",
       "./plugins/**/*.{js,ts}",
-      "./nuxt.config.{js,ts}"
       "./app.vue",
       "./error.vue",
   ],
