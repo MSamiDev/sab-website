534ef35ac07dc054177631523f950d31b9997b83
Mohammad Sami <mohammadsami@duck.com>
2024-03-03 15:59:55 +0530

    Nuxt pages

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-extralight">
-    Hello world!
-  </h1>
-    </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <h1 class="text-3xl font-extralight">
+      Hello world! in Home 
+    </h1>
+      </div>
+  </template>
